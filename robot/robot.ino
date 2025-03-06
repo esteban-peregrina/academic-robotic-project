00741fe388aa59bc7db757b4079dbd7bb7527811
Esteban Peregrina <110783029+esteban-peregrina@users.noreply.github.com>
2025-03-06 09:42:02 +0000

    FEAT: Add ultrasound sensor code
    Allow sensor to work and share data

--- robot/robot.ino
+++ robot/robot.ino
@@ -1,9 +1,39 @@
+#define Broche_Echo 7 // Broche Echo du HC-SR04 sur D7 //
+#define Broche_Trigger 8 // Broche Trigger du HC-SR04 sur D8 //
+
+// Definition des variables
+int MesureMaxi = 300; // Distance maxi a mesurer //
+int MesureMini = 3; // Distance mini a mesurer //
+long Duree;
+long Distance;
+
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(Broche_Trigger, OUTPUT); // Broche Trigger en sortie //
+  pinMode(Broche_Echo, INPUT); // Broche Echo en entree //
+  Serial.begin (115200);
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Debut de la mesure avec un signal de 10 µS applique sur TRIG //
+  digitalWrite(Broche_Trigger, LOW); // On efface l'etat logique de TRIG //
+  delayMicroseconds(2);
+  digitalWrite(Broche_Trigger, HIGH); // On met la broche TRIG a "1" pendant 10µS //
+  delayMicroseconds(10);
+  digitalWrite(Broche_Trigger, LOW); // On remet la broche TRIG a "0" //
+  
+  // On mesure combien de temps le niveau logique haut est actif sur ECHO //
+  Duree = pulseIn(Broche_Echo, HIGH);
+  // Calcul de la distance grace au temps mesure //
+  Distance = Duree*0.034/2; // *** voir explications apres l'exemple de code *** //
+  
+  // Verification si valeur mesuree dans la plage //
+  if (Distance >= MesureMaxi || Distance <= MesureMini) {
+    // Si la distance est hors plage, on affiche un message d'erreur //
+    Serial.println("Distance de mesure en dehors de la plage (3 cm à 3 m)");
+  } else {
+    // Affichage dans le moniteur serie de la distance mesuree //
+    Serial.print("Distance mesuree :");
+    Serial.print(Distance);
+    Serial.println("cm");
+  }
+  delay(1000); // On ajoute 1 seconde de delais entre chaque mesure //
 }
